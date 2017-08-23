de07b41d2020bf9fedd89fc090ccdf38cbdc8859
Daniel Micay <danielmicay@gmail.com>
2017-08-23 03:05:04 -0400

    disable icon normalization

--- src_config/com/android/launcher3/config/FeatureFlags.java
+++ src_config/com/android/launcher3/config/FeatureFlags.java
@@ -23,7 +23,7 @@ public final class FeatureFlags {
     private FeatureFlags() {}
 
     // Custom flags go below this
-    public static boolean LAUNCHER3_DISABLE_ICON_NORMALIZATION = false;
+    public static boolean LAUNCHER3_DISABLE_ICON_NORMALIZATION = true;
     public static boolean LAUNCHER3_LEGACY_FOLDER_ICON = false;
     public static boolean LAUNCHER3_USE_SYSTEM_DRAG_DRIVER = true;
     public static boolean LAUNCHER3_DISABLE_PINCH_TO_OVERVIEW = false;
